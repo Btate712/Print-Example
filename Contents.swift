dde839c86ba0c028f1a413f8a87266e2627cecf9
Bob Tate <bob@Roberts-MacBook-Air.local>
2020-03-30 19:21:32 -0500

    Completed print example with interpolated variables.

--- Contents.swift
+++ Contents.swift
@@ -2,11 +2,9 @@ import UIKit
 
 var str = "Hello, playground"
 
+var myName = "Bob"
 var favoriteWord = "irregardless"
 var favoriteColor = "blue"
 var favoriteMusician = "Laura Jane Grace"
 
-print(favoriteWord)
-print(favoriteColor)
-print(favoriteMusician)
-
+print("Hi, my name is \(myName). My favorite word is \(favoriteWord). My favorite color is \(favoriteColor). My favorite musician is currently \(favoriteMusician)")
