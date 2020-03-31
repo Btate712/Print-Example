033bcc554d9df361f7fc142ef330ba437bbb94fe
Bob Tate <bob@Roberts-MacBook-Air.local>
2020-03-30 19:18:08 -0500

    Completed initial print example

--- Contents.swift
+++ Contents.swift
@@ -5,3 +5,8 @@ var str = "Hello, playground"
 var favoriteWord = "irregardless"
 var favoriteColor = "blue"
 var favoriteMusician = "Laura Jane Grace"
+
+print(favoriteWord)
+print(favoriteColor)
+print(favoriteMusician)
+
